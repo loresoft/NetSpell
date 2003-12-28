f2c41f7446c2802b6ac296673abed923314fb0a5
pwelter34 <pwelter34@1163a8d4-780d-0410-b016-e84d8c4b4503>
2003-12-28 06:45:37 +0000

    Web demo updates
    git-svn-id: http://svn.code.sf.net/p/netspell/code/trunk@116 1163a8d4-780d-0410-b016-e84d8c4b4503

--- NetSpell/demo/web/SimpleCSharp/spell.js
+++ NetSpell/demo/web/SimpleCSharp/spell.js
@@ -3,7 +3,7 @@ var spellURL = "SpellCheck.aspx";
 function checkSpelling() 
 {
     if (window.showModalDialog) 
-        var result = window.showModalDialog(spellURL + "?ModalFrame=true", window, "dialogHeight:320px; dialogWidth:400px; edge:Raised; center:Yes; help:No; resizable:No; status:No; scroll: No");    
+        var result = window.showModalDialog(spellURL + "?Modal=true", window, "dialogHeight:320px; dialogWidth:400px; edge:Raised; center:Yes; help:No; resizable:No; status:No; scroll: No");    
     else
         var newWindow = window.open(spellURL, "newWindow", "height=320,width=400,scrollbars=0,resizable=0,toolbars=0");
 }
